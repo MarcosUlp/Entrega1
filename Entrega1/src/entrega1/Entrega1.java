228cdb775105a7eaf10668e79e5dae5d6f35981e
Marcos Giraudi <crinomarcos@gmail.com>
2023-08-22 10:20:13 -0300

    cambio
    solo es para la entrega 1

---
+++ Entrega1/src/entrega1/Entrega1.java
@@ -0,0 +1,12 @@
+package entrega1;
+
+public class Entrega1 {
+
+
+    public static void main(String[] args) {
+        //El famoso
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
